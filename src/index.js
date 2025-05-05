fae83c07ece84a3248d1b7fcfe2cf32b197dfa6d
Joseph <terzitech@gmail.com>
2025-05-05 13:59:27 -0400

    Refactor command files: move command implementations to core directory and update imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // Central location for importing and exporting all components
 import { createAsciiTree } from '../bin/renderers/asciiTree.js';
 import { renderToConsole } from '../bin/renderers/consoleRenderer.js';
-import { addObject } from './commands/addObject.js';
+import { addObject } from './core/addObject.js';
 import DataService from './services/DataService.js';
 import { computeOutlines } from './utils/computeOutlines.js';
-import { demote } from './commands/demote.js';
-import { promote } from './commands/promote.js';
-import { moveDown } from './commands/moveDown.js';
-import { moveUp } from './commands/moveUp.js';
-import { deleteObject } from './commands/deleteObject.js';
+import { demote } from './core/demote.js';
+import { promote } from './core/promote.js';
+import { moveDown } from './core/moveDown.js';
+import { moveUp } from './core/moveUp.js';
+import { deleteObject } from './core/deleteObject.js';
 
 export { createAsciiTree, 
     renderToConsole, 
