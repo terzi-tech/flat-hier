cd31fd576d61b95a30ec174a416a090d4b7a5210
Joseph Terzi <terzitech@gmail.com>
2025-05-09 11:22:09 -0400

    feat: add init command handling to CLI

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
 import helpCommand from '../src/cli/commands/help.js';
+import initCommand from '../src/cli/commands/init.js';
 
 const args = process.argv.slice(2);
 
 if (args.length === 0 || args.includes('--help') || args.includes('-h')) {
     helpCommand();
+} else if (args[0] === 'init') {
+    initCommand();
 } else {
     console.log('Unknown command. Use --help or -h for usage information.');
 }
